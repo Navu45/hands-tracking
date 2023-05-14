93072484fd11ef6f82097453cae7694dafd52d0f
Alexey Yamoncheryaev <jamoncheryev1@gmail.com>
2023-05-14 21:47:25 +0300

    develop: to_torchscript backbone/neck/head
    - Tested MogaNet/TransformerFCN/ZeroHead on random data
    - saved to data/models/torch-script (test_inference files)

--- feature_extractor/__init__.py
+++ feature_extractor/__init__.py
@@ -1,2 +1,1 @@
 from feature_extractor.model import MogaNet
-from feature_extractor.test import MNIST_dataset_configure, MNISTransform
---
+++ feature_pyramid/__init__.py
@@ -0,0 +1,1 @@
+from feature_pyramid.model import TransformerFCN
